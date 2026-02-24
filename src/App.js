bb08ac3754ddfc3ef5eb0c19270dded97eb41e01
ADaviskiba <sascha.dawiskiba@mail.ru>
2026-02-24 11:49:27 +0700

    Added color menu, header and other

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function ToUpload(){
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    const url = "http://12pol-cab428-01:4433/upload/all";
+    const url = "http://10.4.16.9:4433/upload/all";
 
     fetch(url)
       .then(response => {
@@ -61,7 +61,7 @@ function ToList() {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const url = "http://12pol-cab428-01:4433/cart/all";
+    const url = "http://10.4.16.9:4433/cart/all";
 
     fetch(url)
       .then(response => {
@@ -166,7 +166,7 @@ function ToListMalahova53() {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const url = "http://12pol-cab428-01:4433/cart/all";
+    const url = "http://10.4.16.9:4433/cart/all";
 
     fetch(url)
       .then(response => {
@@ -217,38 +217,57 @@ function ButtonToList() {
   const [activeTable, setActiveTable] = useState(null);
   const [uploadTable, setUploadTable] = useState(null);
 
+
   const handleClick = () => {
     setActiveTable('yurina');
     setUploadTable('');
+    const urina = document.getElementById('ur')
+    urina.style.backgroundColor = "aqua"
+    const malahova51= document.getElementById('mal51')
+    malahova51.style.backgroundColor = "aliceblue"
+    const malahova53 = document.getElementById('mal53')
+    malahova53.style.backgroundColor = "aliceblue"
   }
-
   const handleClick2 = () => {
     setActiveTable('malahova51');
     setUploadTable('');
+    const urina = document.getElementById('ur')
+    urina.style.backgroundColor = "aliceblue"
+    const malahova51= document.getElementById('mal51')
+    malahova51.style.backgroundColor = "aqua"
+    const malahova53 = document.getElementById('mal53')
+    malahova53.style.backgroundColor = "aliceblue"
   }
   const handleClick3 = () => {
     setActiveTable('malahova53');
     setUploadTable('');
+    const urina = document.getElementById('ur')
+    urina.style.backgroundColor = "aliceblue"
+    const malahova51= document.getElementById('mal51')
+    malahova51.style.backgroundColor = "aliceblue"
+    const malahova53 = document.getElementById('mal53')
+    malahova53.style.backgroundColor = "aqua"
   }
 
   return (
     <>
-    <div className={styles.menu}>
-      <button name="urina" className={styles.a} onClick={handleClick}><b>Юрина</b></button>
-      <button name="malahova51" className={styles.a} onClick={handleClick2}><b>Малахова51</b></button>
-      <button name="malahova53" className={styles.a} onClick={handleClick3}><b>Малахова53</b></button>
-    </div>
-    <div>
-      {activeTable==='yurina' && <button name="urina" className={styles.upload} onClick={()=>setUploadTable('upload_yurina')}><b>Загрузить картриджи на заправку</b></button>}
-    </div>
-    <div className={styles.tables}>
-      {activeTable === 'yurina' && <ToList />}
-      {activeTable === 'malahova51' && <ToListMalahova51 />}
-      {activeTable === 'malahova53' && <ToListMalahova53 />}
-      {uploadTable === 'upload_yurina'&&activeTable==='yurina'&& <ToUpload />}
-    </div>
-    <div>
-    </div>
+    <div className={styles.header}><a><b>Картриджи БСМП№2</b></a></div>
+      <div className={styles.menu}>
+        <button name="urina" id='ur' className={styles.a} onClick={handleClick}><b>Юрина</b></button>
+        <button name="malahova51" id = 'mal51' className={styles.a} onClick={handleClick2}><b>Малахова51</b></button>
+        <button name="malahova53" id = 'mal53' className={styles.a} onClick={handleClick3}><b>Малахова53</b></button>
+      </div>
+      <div className={styles.table_upload}>
+        {activeTable==='yurina' && <button name="urina" className={styles.table_upload_but} onClick={()=>setUploadTable('upload_yurina')}><b>Загрузить картриджи на заправку</b></button>}
+      </div>
+      <div className={styles.tables}>
+        
+        {activeTable === 'yurina' && <ToList />}
+        {activeTable === 'malahova51' && <ToListMalahova51 />}
+        {activeTable === 'malahova53' && <ToListMalahova53 />}
+        {uploadTable === 'upload_yurina'&&activeTable==='yurina'&& <ToUpload />}
+      </div>
+
     </>
   );
 }
